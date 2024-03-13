fee90d92a2915c2d9367d34111e4e1af16f14259
j.montoya <j.montoya@bbva.com>
2024-03-13 16:48:52 -0600

    feat: agrega maqueta y estilos del SearchBar component

--- 5-trainers-project/trainers-pokemon-frontend/src/components/SearchBar/index.jsx
+++ 5-trainers-project/trainers-pokemon-frontend/src/components/SearchBar/index.jsx
@@ -1,9 +1,39 @@
 import './index.css';
 
+// import TuneIcon from '@mui/icons-material/Tune';
+
 function SearchBar() {
+
+    const handleForm = (event) => {
+        event.preventDefault();
+        // const name = document.querySelector('#trainer-name');
+        // const isChampion = document.querySelector('#isChampion');
+    }
+
     return (
-        <h1>searchbar works!</h1>
+        <>
+            <div className='container-form'>
+                <form id='search-form' className='form-element' onSubmit={handleForm}>
+
+                    <h2>Nombre del entrenador: </h2>
+                    <input type="text" id="trainer-name" name="trainer-name" placeholder='Nombre entrenador...' />
+                    <div className='radio-container'>
+                        {/* <TuneIcon /> */}
+                        ¿Sólo campeones?:
+                        <label htmlFor="true">Si</label>
+                        <input type="radio" name="isChampion" id="isChampion" value="true" />
+
+                        <label htmlFor="false">No</label>
+                        <input type="radio" name="isChampion" id="isChampion" value="false" />
+                    </div>
+
+                    <input type="submit" className="button-default" value="Buscar" />
+
+                </form>
+            </div>
+        </>
     );
+
 }
 
 export { SearchBar };
